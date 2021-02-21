fa4df0eb2add99659f50466a67782293e502d7de
rroyalty <rroyalty@gmail.com>
2021-02-21 10:08:37 -0500

    Good progress

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-let readmeJSON = {};
-const openMenu = 
-`WELCOME TO RYAN'S README BUILDER!
-    
-New - Create new readme file.
-Preview - Previews file that you're working on in Terminal.
-Preview <section> - Previews the section of the file you're working on.
-Modify <section> - Modifies a specific section of the ReadMe.
-Clear <section> - Clears the section.
-Save - Saves the read me file.
-Help - Displays current commands.
+const menus = require('./menus.json');
 
-`;
+let readmeJSON = {};
 
 const newReadme = () => {
     readmeJSON = {};
@@ -28,54 +18,211 @@ const newReadme = () => {
                 message: 'What sections would you like to include in your ReadMe?',
                 name: 'sections',
                 pageSize: 8,
-                choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
+                choices: ['#Description', '#Installation', '#Usage', '#License', '#Contributing', '#Tests', '#Questions']
             }
         ]).then((data) => {
-            readmeJSON["Title"] = data.title;
-            data.sections.forEach((ele) => readmeJSON[ele] = "");
+            readmeJSON["###Title"] = `###${data.title}`;
+            readmeJSON["##Table of Contents"] = "";
+            data.sections.forEach((ele) => {
+                readmeJSON["##Table of Contents"] = `${readmeJSON["##Table of Contents"]}\n[${ele}](${ele})`;
+                readmeJSON[ele] = "";
+            })
             commandMain();
         }).catch((err) => {
             console.log(err);
-            commandMain();
         })
 }
 
-const previewReadme = (title, sections) => {
+const previewReadme = (title, toc, sections) => {
     const reduced = sections.reduce((accumulator, secEle) => { 
-        return `${accumulator}\n#${secEle[0]}\n${secEle[1]}`
-    }, `##${title[1]}\n`);
+        return `${accumulator}---\n<a name="${secEle[0]}"/>\n${secEle[0]}\n\n${secEle[1]}`
+    }, `${title[1]}\n---\n${toc[0]}\n${toc[1]}\n`);
     return reduced;
 }
 
+const helpMenus = (parameter) => {
+    switch(!parameter === false) {
+        case true:
+            try {
+                if (!menus[parameter.toLowerCase()] === false) console.log(menus[parameter.toLowerCase()]);
+                else  {
+                    console.log("Not a valid help command\n")
+                    console.log(menus.help)
+                }
+            } catch {(err) => {
+                console.log(err)
+                console.log(menus.help)
+                };
+            }
+        break;
+        default:
+            console.log(menus.help);
+    }
+}
+
+const modifyParts = (parameter) => {
+    switch(!parameter === false) {
+        case true:
+            try {
+                if (!menus[parameter.toLowerCase()] === false) console.log(menus[parameter.toLowerCase()]);
+                else  {
+                    console.log("Not a modifiable part\n")
+                    console.log(`#${parameter}`)
+                }
+            } catch {(err) => {
+                console.log(err)
+                };
+            }
+        break;
+        default:
+            console.log(menus[parameter]);
+    }
+}
+
 const commandMain = () => {
-    console.log(openMenu);
+    console.log(menus.base);
     inquirer
         .prompt([{
             type: 'input',
             name: 'command',
             message: 'Enter command: '
         }]).then((data) => {
-            console.log(data.command)
             const dataArr = data.command.split(" ")
-        
-            console.log(dataArr);
-
-            switch(dataArr[0]) {
-                case 'New' || 'NEW' || 'new':
+            switch(dataArr[0].toLowerCase()) {
+                case 'exit':
+                    process.exit(0);
+                break;
+                case 'new':
                     newReadme(readmeJSON);
                 break;
-                case 'Preview' || 'PREVIEW' || 'preview':
+                case 'preview':
                     const array = Object.entries(readmeJSON);
-                    const preview = previewReadme(array[0], array.splice(1, array.length - 1))
-                    console.log(`\n\n${preview}`)
+                    const preview = previewReadme(array[0], array[1], array.splice(2, array.length - 2));
+                    console.log(`\n\n${preview}`);
+                    commandMain();
+                break;
+                case 'help':
+                    helpMenus(dataArr[1]);
+                    commandMain();
+                break;
+                case 'modify':
+                    modifyParts(dataArr[1]);
+                    commandModify();
                 break;
                 default:
+                    console.log("Invalid command.")
                     commandMain();
             }
         }).catch((err) => {
             console.log(err);
-            commandMain();
         })
+}
+
+const commandModify = () => {
+    console.log(menus.base);
+    const array = Object.entries(readmeJSON);
+    const list = array.splice(1, array.length - 1).map(x => x[0]);
+    inquirer
+        .prompt([{
+            type: 'list',
+            name: 'command',
+            message: 'Modify: ',
+            choices: list,
+            pageSize: 8
+        }]).then((data) => {
+            console.log(`#${data.command}`)
+            addPartPrompt(data.command);
+        }).catch((err) => {
+            console.log(err);
+        })
+}
+
+const addPartPrompt = (section) => {
+    inquirer
+        .prompt([{
+            type: 'list',
+            name: 'another',
+            message: 'Add another part?',
+            choices: ["Yes", "No"]
+        }]).then((data) => {
+            if (data.another === "No") commandMain();
+            else {
+                inquirer
+                    .prompt([{
+                        type: 'list',
+                        name: 'part',
+                        message: 'Choose part to add.',
+                        choices:["Paragraph","Link","Image"]
+                    }]).then((data) => {
+                        part2add(data.part, section)
+                        })
+            } 
+        }).catch((err) => {
+            console.log(err);
+        })
+}
+
+const part2add = (part, section) => {
+    switch (part) {
+        case "Paragraph":
+            inquirer
+                .prompt([{
+                    type: 'input',
+                    name: 'part',
+                    message: 'New Paragraph',
+                }]).then((data) => {
+                    addPart(data.part, section)
+                    })
+        break;
+        case "Link":
+            inquirer
+                .prompt([{
+                    type: 'input',
+                    name: 'alt',
+                    message: '[Site Name]: ',
+                }]).then((data) => {
+                    const alt = data.alt;
+                    inquirer
+                    .prompt([{
+                        type: 'input',
+                        name: 'url',
+                        message: '(URL): ',
+                    }]).then((data, url) => {
+                            addPart(`[${alt}](${data.url})`, section)
+                        })
+
+                    })
+        break;
+        case "Image":
+            inquirer
+                .prompt([{
+                    type: 'input',
+                    name: 'alt',
+                    message: '![Alt Text]: ',
+                }]).then((data) => {
+                    const alt = data.alt;
+                    inquirer
+                    .prompt([{
+                        type: 'input',
+                        name: 'url',
+                        message: '(Image Source): ',
+                    }]).then((data, url) => {
+                            addPart(`![${alt}](${data.url})`, section)
+                        })
+                    })
+        break;
+    }
+}
+
+const addPart = (part, section) => {
+
+    if (part === "Paragraph") readmeJSON[section] = `${readmeJSON[section]}${part}\n\n`
+    else readmeJSON[section] = readmeJSON[section] = `${readmeJSON[section]}${part}\n`
+    const array = Object.entries(readmeJSON);
+    const preview = previewReadme(array[0], array.splice(1, array.length - 1));
+    console.log(`\n\n${preview}`);
+    addPartPrompt(section);
+
 }
 
 commandMain();
